c3eaef80fc08e4fe15983ab76d459bb0b4a7944b
AdelinaCostea6 <adelina.costea@student.unitbv.ro>
2024-11-01 19:53:11 +0200

    added constructor that initializes bomb position

---
+++ Bomb.cpp
@@ -0,0 +1,7 @@
+#include "Bomb.h"
+
+Bomb::Bomb(int x, int y)
+{
+	position.first = x;
+	position.second = y;
+}
--- Bomb.h
+++ Bomb.h
@@ -1,9 +1,13 @@
 #pragma once
 #include <iostream>
+
 class Bomb
 {
 private:
 	int radius;
 	std::pair<int, int>position;
+
+public:
+	Bomb(int x, int y);
 };
 
