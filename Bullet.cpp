0f7a2bc1b5d5bb9c60385b8f242b10a5c2c97445
GhiuzanRoxana <roxanaghiuzan18@gmail.com>
2024-11-01 23:50:16 +0200

    Resolve bugs from class bullet

--- Bullet.cpp
+++ Bullet.cpp
@@ -1,12 +1,5 @@
 #include "Bullet.h"
 
-Bullet::Bullet()
-{
-	this->direction = 0;
-	this->position.first = 0;
-	this->position.second = 0;
-	this->speed = 0, 25;
-}
 Bullet::Bullet() :startPosition({ 0,0 }), endPosition({0,0}),direction({0}),speed({0}) {}
 
 Bullet::Bullet(std::pair<int, int> startPos, std::pair<int, int> endPos, int directionBullet, int speedBullet):
