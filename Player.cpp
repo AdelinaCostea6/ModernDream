fe3af7dc6249994263443fd0ef3a2910b8047f30
GhiuzanRoxana <roxanaghiuzan18@gmail.com>
2024-11-06 10:46:40 +0200

    Function "loggin" added in Player

--- Player.cpp
+++ Player.cpp
@@ -1,13 +1,10 @@
 #include "Player.h"
 
-Player::Player(std::string name, Weapon *weapon, std::pair<int,int>position)
+Player::Player(std::string name, Weapon* weapon, std::pair<int, int>position) :
+	name(name), weapon(weapon), position(position), points(0), lives(3){}
+
+void Player::login()
 {
-	this->name = name;
-	this->position.first = position.first;
-	this->position.second = position.second;
-	this->points = 0;
-	this->lives = 3;
-	this->weapon = weapon;
-
-
+	std::cout << "Player" << name << "logged in" << std::endl;
 }
+
