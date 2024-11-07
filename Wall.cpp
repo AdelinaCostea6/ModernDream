f27c285c3d5ecd98ad96d51682c3e0148ad88379
delaiemma <emma.de@student.unitbv.ro>
2024-11-07 16:59:03 +0200

    implementation of a function to reduce durability

--- Wall.cpp
+++ Wall.cpp
@@ -1,1 +1,33 @@
 #include "Wall.h"
+
+std::pair<int, int> getPosition()
+{
+    return position;
+}
+
+int getType()
+{
+    return type;
+}
+
+int getDurability()
+{
+    return durability;
+}
+
+bool getDestructible()
+{
+    return destructible;
+}
+
+void reduceDurability(int amount)
+{
+    if (this.destructible)
+    {
+        this.durability = this.durablility - amount;
+    }
+    if (this.durablility < 0)
+    {
+        this.durability = 0;
+    }
+}
