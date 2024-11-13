d715d69d859d79c932a940041e037c3482b8709c
delaiemma <emma.de@student.unitbv.ro>
2024-11-13 22:23:18 +0200

    defined getters for class Game

--- Game.h
+++ Game.h
@@ -9,11 +9,14 @@ private:
     Map map;
     std::vector<Player> players;
     std::vector<Bomb> bombs;
-public:
-    void DetermineWinner();
-    void WinGame(Player* player);
-    void FinishSecond(Player* player);
-    void CheckAndApplyWeaponUpgrade();
-    std::vector<Player>& GetPlayers() { return players; } 
-};
 
+public:
+    Map GetMap();
+    std::vector<Player> GetPlayers();
+    std::vector<Bomb> GetBombs();
+    void DetermineWinner();
+    void WinGame(Player *player);
+    void FinishSecond(Player *player);
+    void CheckAndApplyWeaponUpgrade();
+    std::vector<Player> &GetPlayers() { return players; }
+};
