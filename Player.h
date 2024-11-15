1c6d8be2e19600ae176e7d5e65c4e2c79020ff2f
GhiuzanRoxana <roxanaghiuzan18@gmail.com>
2024-11-15 13:25:13 +0200

    Player was saved

--- Player.h
+++ Player.h
@@ -11,25 +11,28 @@ private:
 	int points;
 	int lifes;
 	std::pair<int, int> position;
+	std::pair<int, int>initialPosition;
 	int score;
-	bool speedBoostUsed;
+	bool speedBoostUsed=false;
 public:
-	Player(std::string name, std::shared_ptr<class Weapon> weapon, std::pair<int,int>position); 
+	Player(std::string name, std::shared_ptr<class Weapon> weapon, std::pair<int, int> position);
 	void Login();
 	void Movement();
 	void Shoot();
 	void ResetPosition();
 	void hit();
+
 	int GetLifes()const;
 	int GetPoints()const;
 	std::pair<int, int>GetPosition()const;
 	const std::string& GetName();
 	int GetScore();
+	Weapon& GetWeapon();
+
 	void SetScore(int newScore);
 	void SetPoints(int newPoints);
 	bool IsSpeedBoostUsed()const;
 	void SetSpeedBoostUsed(bool used);
-	Weapon& GetWeapon();
 
 
 };
