a35a6de332e3f356fdf2db0337700cce0f3a941c
GhiuzanRoxana <roxanaghiuzan18@gmail.com>
2024-11-15 13:53:33 +0200

    Bomb and bullet was tested

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include "Bomb.h"
+#include "Bullet.h"
 
 int main()
 {
+	std::cout << "===Testing classes===\n\n";
+
+	//bomb test
+	std::cout << "--- Bomb Test ---\n";
+	Coordinates bombPos = { 5, 5 };
+	Bomb bomb(bombPos);
+	std::cout << "Bomb was created at position(" << bomb.GetPosition().first << "," << bomb.GetPosition().second << ")\n";
+	std::cout << "Bomb status : " << (bomb.GetStatus() ? "active" : "inactive") << "\n\n";
+	
+	//bullet test
+	std::cout << "--- Bullet Test ---\n";
+	Bullet bullet({ 1, 1 });
+	std::cout << "A bullet was created at position: (" << bullet.GetPosition().first
+		<< ", " << bullet.GetPosition().second << ")\n";
+	std::cout << "Bullet speed: " << bullet.GetSpeed() << "\n";
+	bullet.Movement();
+	bullet.SetDoubleSpeed();
+	std::cout << "Speed after doublation: " << bullet.GetSpeed() << "\n\n";
+
+
+
 
 }
