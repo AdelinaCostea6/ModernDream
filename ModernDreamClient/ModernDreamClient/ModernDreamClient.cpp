d19d4e44b4e4a7847a97f942e46df07fea385f25
delaiemma <emma.de@student.unitbv.ro>
2024-11-22 17:40:01 +0200

    Add Login Dialog with SQLite ORM Integration for User Authentication

--- ModernDreamClient/ModernDreamClient/ModernDreamClient.cpp
+++ ModernDreamClient/ModernDreamClient/ModernDreamClient.cpp
@@ -1,4 +1,13 @@
 #include "ModernDreamClient.h"
+#include <QApplication>
+#include <QMainWindow>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QMessageBox>
+#include <QTabWidget>
+#include <sqlite_orm/sqlite_orm.h>
 
 ModernDreamClient::ModernDreamClient(QWidget *parent)
     : QMainWindow(parent)
@@ -7,4 +16,33 @@ ModernDreamClient::ModernDreamClient(QWidget *parent)
 }
 
 ModernDreamClient::~ModernDreamClient()
-{}
+{
+}
+
+class LoginDialog : public QDiaglog
+{
+    Q_OBJECT
+
+private:
+    QLineEdit *usernameEdit;
+    QLineEdit *passwordEdit;
+    QPushButton *loginButton;
+    QPushButton *registerButton;
+
+    auto createStorage()
+    {
+        using namespace sqlite_orm;
+        return make_storage("battle_city_users.sqlite",
+                            make_table("users",
+                                       make_column("username", &UserData::username,
+                                                   primary_key()),
+                                       make_column("password",
+                                                   &UserData::password)));
+    }
+
+    struct UserData
+    {
+        std::string username;
+        std::string password;
+    };
+}
