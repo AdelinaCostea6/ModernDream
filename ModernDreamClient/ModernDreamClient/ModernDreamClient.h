e910295ef26315813a092aced3c2d0267bc44242
delaiemma <emma.de@student.unitbv.ro>
2024-11-22 18:28:54 +0200

    Define LoginDialog class and add necessary members

--- ModernDreamClient/ModernDreamClient/ModernDreamClient.h
+++ ModernDreamClient/ModernDreamClient/ModernDreamClient.h
@@ -2,7 +2,10 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_ModernDreamClient.h"
+#include <QlineEdit>
+#include <QPushbutton>
 
+class LoginDialog;
 class ModernDreamClient : public QMainWindow
 {
     Q_OBJECT
@@ -11,6 +14,22 @@ public:
     ModernDreamClient(QWidget *parent = nullptr);
     ~ModernDreamClient();
 
+    LoginDialog(QWidget *parent = nullptr);
+
 private:
     Ui::ModernDreamClientClass ui;
+    QLineEdit *usernameEdit;
+    QLineEdit *passwordEdit;
+    QPushButton *loginButton;
+    QPushButton *registerButton;
+    auto CreateStorage();
+    struct UserData
+    {
+        std::string username;
+        std::string password;
+    };
+
+private slots:
+    void OnLogin();
+    void OnRegister();
 };
