d4ebb38dedd7502726881d1a8a7f1aba0b7f7d6b
AdelinaCostea6 <adelina.costea@student.unitbv.ro>
2024-12-06 19:52:43 +0200

    improved the database

--- ModernDreamServer/ModernDream/DatabaseManager.h
+++ ModernDreamServer/ModernDream/DatabaseManager.h
@@ -11,7 +11,7 @@ namespace sql = sqlite_orm;
 inline auto CreateStorage(const std::string& filename)
 {
 	return sql::make_storage(filename,
-		sql::make_table("users",
+		sql::make_table("players",
 			sqlite_orm::make_column("username", &DataUser::username, sqlite_orm::primary_key()),
 			sqlite_orm::make_column("score", &DataUser::score),
 			sqlite_orm::make_column("points", &DataUser::points),
@@ -35,7 +35,7 @@ public:
 	void DeleteUser(const std::string& username);
 
 private:
-	const std::string kDbFile{ "vanguard.sqlite" };
+	const std::string kDbFile{ "Titans_vanguard.sqlite" };
 
 private:
 	Storage m_db = CreateStorage(kDbFile);
