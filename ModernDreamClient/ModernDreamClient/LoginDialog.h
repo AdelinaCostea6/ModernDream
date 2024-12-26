94d592f42e2bef2052bfd630a152778f49a500c9
delaiemma <emma.de@student.unitbv.ro>
2024-12-26 18:03:07 +0200

    Added layer for onCarMap

--- ModernDreamClient/ModernDreamClient/LoginDialog.h
+++ ModernDreamClient/ModernDreamClient/LoginDialog.h
@@ -9,7 +9,7 @@
 #include <QLineEdit>
 #include <QPushButton>
 #include <QMessageBox>
-#include <QStackedWidget> 
+#include <QStackedWidget>
 #include <sqlite_orm/sqlite_orm.h>
 #include "HttpClient.h"
 
@@ -24,44 +24,42 @@ class LoginDialog : public QDialog
     Q_OBJECT
 
 public:
-    LoginDialog(QWidget* parent = nullptr);
+    LoginDialog(QWidget *parent = nullptr);
 
 protected:
-    void paintEvent(QPaintEvent* event) override;
+    void paintEvent(QPaintEvent *event) override;
 
 private:
-   
-    HttpClient* httpClient;
-    QLineEdit* usernameEdit;
-    QLineEdit* passwordEdit;
-    QPushButton* loginButton;
-    QPushButton* registerButton;
+    HttpClient *httpClient;
+    QLineEdit *usernameEdit;
+    QLineEdit *passwordEdit;
+    QPushButton *loginButton;
+    QPushButton *registerButton;
 
     QPixmap background;
     QPixmap currentBackground;
 
-    QWidget* loginView;
-    QWidget* menuView;
-    QStackedWidget* stackedWidget; 
+    QWidget *loginView;
+    QWidget *menuView;
+    QStackedWidget *stackedWidget;
 
     auto createStorage();
 
     void switchToMenu();
-
 
 private slots:
     void OnLogin();
     void OnRegister();
     void onStartGame();
     void onOptions();
-    void onHelicopterSelected(); 
+    void onCarMap();
+    void onHelicopterSelected();
     void onBoatSelected();
     void onCarSelected();
-    void onLoginSuccess(const QString& username, int score);
+    void onLoginSuccess(const QString &username, int score);
     void onRegisterSuccess();
-    void onLoginFailure(const QString& error);
-    void onRegisterFailure(const QString& error);
-    void showMessageDialog(const QString& message, const QString& color);
-
+    void onLoginFailure(const QString &error);
+    void onRegisterFailure(const QString &error);
+    void showMessageDialog(const QString &message, const QString &color);
 };
 #endif
